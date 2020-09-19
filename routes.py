57273e4575287008ba095a8dcd9539b344796c16
Jami Heljomaa <mail@jami.one>
2020-09-19 08:39:54 +0300

    registration added, not working

--- routes.py
+++ routes.py
@@ -11,7 +11,7 @@ def index():
 
 @app.route("/category/<catName>")
 def category(catName):
-    print (catName)
+    
     catList = categories.get_all()
     thList = threads.get_byCat(catName)
     return render_template("category.html",
@@ -22,9 +22,22 @@ def login():
     if request.method == "GET":
         return render_template("login.html")
     if request.method == "POST":
-        uname = request.form["username"]
-        pword = request.form["password"]
+        uname = request.form["uname"]
+        pword = request.form["pword"]
+        print("uname:", uname, "pword:", pword)
         if users.login(uname, pword):
             return redirect("/")
         else:
             return render_template("login.html", message="Login failed")
+            
+@app.route("/register", methods=["get", "post"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        uname = request.form["uname"]
+        pword = request.form["pword"]
+        if users.register(uname,pword):
+            return redirect("/")
+        else:
+            return render_template("register.html",message="Registration failure")
